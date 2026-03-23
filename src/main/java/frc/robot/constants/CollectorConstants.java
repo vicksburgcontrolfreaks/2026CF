a1dfb814a0131191905cbd2bf010c2754cefd57f
vicksburgcontrolfreaks <90581349+vicksburgcontrolfreaks@users.noreply.github.com>
2026-03-23 17:14:06 -0400

    Added hopper (maybe) function

--- src/main/java/frc/robot/constants/CollectorConstants.java
+++ src/main/java/frc/robot/constants/CollectorConstants.java
@@ -3,9 +3,12 @@ package frc.robot.constants;
 public class CollectorConstants {
   public static final int kUpperCollectorMotorId = 10;
   public static final int kLowerCollectorMotorId = 11;
-  public static final int kLimitSwitchDIO = 0;
+  public static final int kHopperMotorId = 12;
 
   public static final int kMotorCurrentLimit = 40;
 
   public static final double kCollectorSpeed = 0.2;
+
+  public static final double kUpPosition = 0;
+  public static final double kDownPosition = 0.180;
 }
